071bc4cbda788b5e51be37da1c9e41e311a4bf1a
cktjdgus45 <cktjdgus45@naver.com>
2019-12-17 00:12:17 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customer from './component/Customer'
+
+
+class App extends Component {
+  render(){
+  return (
+    <div className="gray-background">
+      <Customer/>
+      
+      
+    </div>
+  );
+ }
+}
+
+export default App;
---
+++ src/component/Customer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render(){
+        return(
+             <div>
+                <h1> ssss </h1> 
+             </div>
+        )
+ }
+}
+
+export default Customer;
