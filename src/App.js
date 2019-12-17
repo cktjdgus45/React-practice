6302eb2a8e9037d7fd4c149571649bf577de8974
cktjdgus45 <cktjdgus45@naver.com>
2019-12-17 14:59:08 +0900

    Apply Muterial ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Customer from './component/Customer'
+import Customer from './component/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop : theme.spacing.unit *3,
+    overFlowX: "auto"
+  },
+  table:{
+    minWidth:1000
+  }
+})
 
 const customers =[
   {
@@ -31,21 +48,20 @@ const customers =[
 ]
 class App extends Component {
   render(){
+    const {classes} =this.props;
   return (
-    <div>
-      { customers.map(customers => {
-        return(
-          <Customer 
-          key={customers.id}
-          id={customers.id} 
-          image={customers.image}
-           name={customers.name} 
-           birthday={customers.birthday}
-           gender={customers.gender}
-           job={customers.job} />
-        )
-      } ) }
-    </div>
+    <Paper className={classes.root}>
+     <Table className={classes.table}>
+       <TableHead>
+         <TableRow>
+           <TableCell>번호</TableCell><TableCell>이미지</TableCell><TableCell>이름</TableCell><TableCell>생년월일</TableCell><TableCell>성별</TableCell><TableCell>직업</TableCell>
+         </TableRow>
+       </TableHead>
+       <TableBody>
+        {customers.map(customers=>{return(<Customer key={customers.id} id={customers.id}  image={customers.image}  name={customers.name}   birthday={customers.birthday}   gender={customers.gender}   job={customers.job} />)})}
+       </TableBody> 
+     </Table>
+    </Paper>
   );}
   }
-  export default App;
+  export default withStyles(styles)(App);
