3b463be9e22fe29b2fc453f6b6e309cea563687c
cktjdgus45 <cktjdgus45@naver.com>
2019-12-20 00:48:44 +0900

    Create Customer Database Table&Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+
 app.get('/api/customers',(req,res)=> {
-    res.send([
-            {
-            'id':1,
-            'image':'http://placeimg.com/64/64/1',
-            'name' : '홍길동',
-            'birthday': '971022',
-            'gender': 'male',
-            'job': 'student'
-             },
-            {
-            'id':2,
-            'image':'http://placeimg.com/64/64/2',
-            'name' : '김유동',
-            'birthday': '960922',
-            'gender': 'male',
-            'job': 'professor'
-             },
-             {
-            'id':3,
-            'image':'http://placeimg.com/64/64/3',
-            'name' : '천유동',
-            'birthday': '920421',
-            'gender': 'male',
-            'job': 'programmer'
-            }
-    ])
-})
+    connection.query(
+        "SELECT*FROM CUSTOMER",(err,rows,fields)=>{
+            res.send(rows);
+        }
+    );
+});
 app.listen(port,()=>console.log(`Listening on port ${port}`));
